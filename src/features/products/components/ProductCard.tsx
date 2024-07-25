41dbe9efca3fd87555ad64c6916ef9d0eecd1bd3
Dereje Seifu <Derejeseifu3030@gmail.com>
2024-07-25 16:13:16 +0300

    updated

--- src/features/products/components/ProductCard.tsx
+++ src/features/products/components/ProductCard.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image';
 import { Product } from '@/models/product';
 import { useAppDispatch } from '@/store/app-store-hooks';
 import { addItem } from '../../cart/store/cart-slice';
+import Link from 'next/link';
 
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   const dispatch = useAppDispatch();
@@ -20,23 +21,27 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
       <CardHeader>
         <CardTitle className='text-sm text-primaryT font-normal'>FEATURED</CardTitle>
       </CardHeader>
-      <CardContent className='relative flex flex-col items-center w-full gap-8'>
-        <Image 
-          src={product.imageUrl} 
-          width={200} 
-          height={200} // Adjusted height for better display
-          alt={product.name} // Added alt text for better accessibility
-          quality={75} // Added quality for better image quality control
-          className='object-contain' // Added class for better image fit
-        />
-        <p className='text-md text-center font-medium leading-relaxed px-4'>
-          {product.name}
-        </p>
-        <p>
-          <span className='text-md mr-2 line-through'>ETB 4000</span>
-          {product.price}
-          <span className='text-primaryT ml-2'>25% Off</span>
-        </p>
+      <CardContent>
+        <Link target='_blank' href={`/products/${product.id}`}>
+          <div className='relative flex flex-col items-center w-full gap-8'>
+          <Image 
+            src={product.imageUrl} 
+            width={200} 
+            height={200} // Adjusted height for better display
+            alt={product.name} // Added alt text for better accessibility
+            quality={75} // Added quality for better image quality control
+            className='object-contain' // Added class for better image fit
+          />
+          <p className='text-md text-center font-medium leading-relaxed px-4'>
+            {product.name}
+          </p>
+          <p>
+            <span className='text-md mr-2 line-through'>ETB 4000</span>
+            {product.price}
+            <span className='text-primaryT ml-2'>25% Off</span>
+          </p>
+          </div>
+        </Link>
       </CardContent>
       <CardFooter className='w-full flex gap-2'>
         <Button variant={'outline'} className=''>Buy Now</Button>
